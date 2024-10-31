0a25dd21c11dacc27ad63431bd4cc8e8104fe9d2
Katarzyna Jaworska <161602727+katjaworska@users.noreply.github.com>
2024-10-31 15:01:12 +0100

    Update ukulele_chords.py
    Class Chords moved to separate file Chords.py

--- ukulele_chords.py
+++ ukulele_chords.py
@@ -1,47 +1,4 @@
-class Chords:
-    name = ""
-    grip = {}
-
-    def display(self):
-        """displays a single chord"""
-        frets = [1, 2, 3, 4]
-        strings = ['G', 'C', 'E', 'A']
-        print(f"** {self.name} **\n_______")
-        for fret in frets:
-            i = 0
-            for _ in strings:
-                if self.grip[fret][i] == 1 and i != 3:
-                    print("O_", end="")
-                elif self.grip[fret][i] == 1 and i == 3:
-                    print("O")
-                elif self.grip[fret][i] == 0 and i != 3:
-                    print("|_", end="")
-                else:
-                    print("|")
-                i += 1
-        print()
-
-    def generate(self, file):
-        """generates a single chord to file"""
-        frets = [1, 2, 3, 4]
-        strings = ['G', 'C', 'E', 'A']
-        file.write(f"** {self.name} **\n_______\n")
-        for fret in frets:
-            i = 0
-            list1 = []
-            for _ in strings:
-                if self.grip[fret][i] == 1 and i != 3:
-                    list1.append("O_")
-                elif self.grip[fret][i] == 1 and i == 3:
-                    list1.append("O")
-                elif self.grip[fret][i] == 0 and i != 3:
-                    list1.append("|_")
-                else:
-                    list1.append("|")
-                i += 1
-            list1.append("\n")
-            file.writelines(list1)
-        file.writelines("\n")
+from Chords import Chords
 
 
 def homepage():
@@ -63,6 +20,7 @@ def homepage():
             print("Goodbye!")
         case _:
             pass
+
 
 def showing_chords():
     """displays list of chords which user wants"""
@@ -87,9 +45,12 @@ def showing_chords():
                     chord.name = i
                     space_pos = line.rfind(" ")
                     chord.grip = {
-                        1: [int(line[space_pos + 2]), int(line[space_pos + 3]), int(line[space_pos + 4]), int(line[space_pos + 5])],
-                        2: [int(line[space_pos + 8]), int(line[space_pos + 9]), int(line[space_pos + 10]), int(line[space_pos + 11])],
-                        3: [int(line[space_pos + 14]), int(line[space_pos + 15]), int(line[space_pos + 16]), int(line[space_pos + 17])],
+                        1: [int(line[space_pos + 2]), int(line[space_pos + 3]), int(line[space_pos + 4]),
+                            int(line[space_pos + 5])],
+                        2: [int(line[space_pos + 8]), int(line[space_pos + 9]), int(line[space_pos + 10]),
+                            int(line[space_pos + 11])],
+                        3: [int(line[space_pos + 14]), int(line[space_pos + 15]), int(line[space_pos + 16]),
+                            int(line[space_pos + 17])],
                         4: [int(line[space_pos + 20]), int(line[space_pos + 21]), int(line[space_pos + 22]),
                             int(line[space_pos + 23])]}
                     chord.display()
@@ -131,6 +92,7 @@ def generate_all_to_file():
     new_file.close()
     print("All chords have been exported to external file 'ukulele_all_chords.txt'\n")
     homepage()
+
 
 print("Welcome! What would you like to do today?")
 homepage()
